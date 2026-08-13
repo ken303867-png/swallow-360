56ec050dd55c4630528714aa02f349b5a13b8b7b
ken303867-png <ken303867@gmail.com>
2026-08-13 21:07:04 +0900

    Refresh 360 backup cache

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 "use strict";
-const CACHE='swallow-360-v2-transfer';
+const CACHE='swallow-360-v3-backup';
 const SHELL=['./','./index.html','./manifest.webmanifest','./icon.svg','./app.html.gz','./data-transfer.js'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>(k.startsWith('swallow-360-')||k.startsWith('dysphagia-360-pwa-'))&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
